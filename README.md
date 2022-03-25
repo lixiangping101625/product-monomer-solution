9b3708c383714f54f6cdf08c3dedb068b7387001
lixiangping <admin>
2022-03-25 16:02:42 +0800

    commit

--- README.md
+++ README.md
@@ -189,3 +189,77 @@
                 order.setTitle("测试订单");
                 TOrder saveOrder = orderRepository.save(order);
                 //5、新增订单详情
+                
+        方案三：使用lock + 手动事务 解决超卖问题
+            /**
+             * 使用JDK1.5提供的可重入锁ReentrantLock + 手动事务 解决超卖问题：
+             *      注意：
+             *          1、使用lock锁需要手动释放锁
+             *          2、强烈建议配合try..finally..使用
+             *               lock.lock();
+             *               try{
+             *                   代码块
+             *               }finally{
+             *                   lock.unLock();
+             *               }
+             */
+            private Lock lock = new ReentrantLock();
+            public void orderPlace() {
+                TProduct product = null;
+        
+                //加锁
+                lock.lock();
+                try {
+                    //手动开启事务！
+                    TransactionStatus transactionStatus1 = transactionManager.getTransaction(transactionDefinition);
+                    //1、查询库存
+                    product = productRepository.getById(purchaseProductId);
+                    if (product==null) {
+                        //异常事务回滚
+                        transactionManager.rollback(transactionStatus1);
+                        throw new RuntimeException("商品不存在");
+                    }
+                    Integer currentCount = product.getCount();
+                    //2、库存校验&修改库存
+                    if (purchaseProductCount > currentCount) {
+                        //异常事务回滚
+                        transactionManager.rollback(transactionStatus1);
+                        throw new RuntimeException("商品仅剩" + currentCount + " 件，无法购买~");
+                    }
+                    Integer leftCount = currentCount - purchaseProductCount;
+                    //3、更新库存（如果采用增量方式修改商品库存，就会出现库存为负数的场景。这里是直接修改库存，所以尽管商品库存正常（0），但实际上是创建了5个订单，还是超卖）
+                    product.setCount(leftCount);
+                    TProduct saveProduct = productRepository.save(product);
+        
+                    transactionManager.commit(transactionStatus1);
+                } finally {
+                    lock.unlock();
+                }
+        
+                /*
+                 * 精良避免事务嵌套！！！
+                 */
+                //手动开启事务！
+                TransactionStatus transactionStatus = transactionManager.getTransaction(transactionDefinition);
+                //4、新增订单
+                TOrder order = new TOrder();
+                order.setPrice(product.getPrice().multiply(BigDecimal.valueOf(purchaseProductCount)));
+                order.setTitle("测试订单");
+                TOrder saveOrder = orderRepository.save(order);
+                //5、新增订单详情
+                TOrderItem orderItem = new TOrderItem();
+                orderItem.setCount(purchaseProductCount);
+                orderItem.setOrderId(saveOrder.getId());
+                orderItem.setProductId(purchaseProductId);
+                TOrderItem saveOrderItem = orderItemRepository.save(orderItem);
+                System.out.println("下单成功，订单id：" + saveOrder.getId());
+                //提交事务
+                transactionManager.commit(transactionStatus);
+            }
+        
+        补充：以上解决方案其实是悲观锁方案，通俗的说就是让查询库存和修改库存在一个事务内进行，这样就可以保证操作的原子性。 
+                乐观锁参考：
+                    update Sku s 
+                    set s.stock = s.stock-:count
+                    where s.id=:id
+                    and s.stock >= :count
--- src/main/java/com/hlkj/productmonomersolution/service/OrderService.java
+++ src/main/java/com/hlkj/productmonomersolution/service/OrderService.java
@@ -15,6 +15,8 @@ import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author Lixiangping
@@ -122,12 +124,14 @@ public class OrderService {
         transactionManager.commit(transactionStatus);
     }*/
 
+
+
     /**
      * synchronized(){代码块：主要造成库存充足假象的代码块} + 手动事务  解决超卖问题：
      *      synchronized (this){代码块} 和 synchronized (当前类对象属性){代码块} 一样，因为Springboot默认是单例的。这里示例就采用synchronized (this){代码块}的方式。
      *      synchronized (当前类){代码块} 最安全。因为类只有一个，而对象特殊情况下可能会创建多个，所以不是太安全。
      */
-    public void orderPlace() {
+    /*public void orderPlace() {
         TProduct product = null;
         synchronized (this){
             //手动开启事务！
@@ -153,6 +157,73 @@ public class OrderService {
 
             transactionManager.commit(transactionStatus1);
         }
+        *//*
+         * 精良避免事务嵌套！！！
+         *//*
+        //手动开启事务！
+        TransactionStatus transactionStatus = transactionManager.getTransaction(transactionDefinition);
+        //4、新增订单
+        TOrder order = new TOrder();
+        order.setPrice(product.getPrice().multiply(BigDecimal.valueOf(purchaseProductCount)));
+        order.setTitle("测试订单");
+        TOrder saveOrder = orderRepository.save(order);
+        //5、新增订单详情
+        TOrderItem orderItem = new TOrderItem();
+        orderItem.setCount(purchaseProductCount);
+        orderItem.setOrderId(saveOrder.getId());
+        orderItem.setProductId(purchaseProductId);
+        TOrderItem saveOrderItem = orderItemRepository.save(orderItem);
+        System.out.println("下单成功，订单id：" + saveOrder.getId());
+        //提交事务
+        transactionManager.commit(transactionStatus);
+    }*/
+
+
+    /**
+     * 使用JDK1.5提供的可重入锁ReentrantLock + 手动事务 解决超卖问题：
+     *      注意：
+     *          1、使用lock锁需要手动释放锁
+     *          2、强烈建议配合try..finally..使用
+     *               lock.lock();
+     *               try{
+     *                   代码块
+     *               }finally{
+     *                   lock.unLock();
+     *               }
+     */
+    private Lock lock = new ReentrantLock();
+    public void orderPlace() {
+        TProduct product = null;
+
+        //加锁
+        lock.lock();
+        try {
+            //手动开启事务！
+            TransactionStatus transactionStatus1 = transactionManager.getTransaction(transactionDefinition);
+            //1、查询库存
+            product = productRepository.getById(purchaseProductId);
+            if (product==null) {
+                //异常事务回滚
+                transactionManager.rollback(transactionStatus1);
+                throw new RuntimeException("商品不存在");
+            }
+            Integer currentCount = product.getCount();
+            //2、库存校验&修改库存
+            if (purchaseProductCount > currentCount) {
+                //异常事务回滚
+                transactionManager.rollback(transactionStatus1);
+                throw new RuntimeException("商品仅剩" + currentCount + " 件，无法购买~");
+            }
+            Integer leftCount = currentCount - purchaseProductCount;
+            //3、更新库存（如果采用增量方式修改商品库存，就会出现库存为负数的场景。这里是直接修改库存，所以尽管商品库存正常（0），但实际上是创建了5个订单，还是超卖）
+            product.setCount(leftCount);
+            TProduct saveProduct = productRepository.save(product);
+
+            transactionManager.commit(transactionStatus1);
+        } finally {
+            lock.unlock();
+        }
+
         /*
          * 精良避免事务嵌套！！！
          */
@@ -173,5 +244,4 @@ public class OrderService {
         //提交事务
         transactionManager.commit(transactionStatus);
     }
-
 }
